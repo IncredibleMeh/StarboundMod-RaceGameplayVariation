5bdd43cbf10d43e873e1d79ce93beb7a7f4813b6
Eric Carpe <incrediblemeh@gmail.com>
2015-11-30 20:51:02 -0500

    Consumable Update
    Further completion of additions to effects consumables have on races.

--- RaceGameplayVariation/stats/effects/food/meatyfood/meatyfoodheal.lua
+++ RaceGameplayVariation/stats/effects/food/meatyfood/meatyfoodheal.lua
@@ -5,7 +5,13 @@ function init()
 
   script.setUpdateDelta(5)
 
-  self.healingRate = effect.configParameter("healAmount", 30) / effect.duration()
+  if world.entitySpecies(entity.id()) == "glitch" then
+		self.healingRate = effect.configParameter("healAmount", 30) / 2 / effect.duration()
+	elseif world.entitySpecies(entity.id()) == "floran" then
+		self.healingRate = effect.configParameter("healAmount", 30) * 1.5 / effect.duration()
+	else
+		self.healingRate = effect.configParameter("healAmount", 30) / effect.duration()
+	end
 end
 
 function update(dt)
