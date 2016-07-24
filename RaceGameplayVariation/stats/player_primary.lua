c6e14f842cc10b40e7cb1e09b5e4552121157748
Eric Carpe <incrediblemeh@gmail.com>
2016-07-24 04:18:05 -0400

    Bug fixes and minor updates
    Small step forwards towards cleaning the mod up for 1.0

--- RaceGameplayVariation/stats/player_primary.lua
+++ RaceGameplayVariation/stats/player_primary.lua
@@ -1,18 +1,49 @@
+require "/scripts/status.lua"
+require "/scripts/achievements.lua"
+
 function init()
   self.lastYPosition = 0
   self.lastYVelocity = 0
   self.fallDistance = 0
   self.hitInvulnerabilityTime = 0
-	self.shieldHitInvulnerabilityTime = 0
-  self.damageFlashTime = 0
+  self.shieldHitInvulnerabilityTime = 0
   self.suffocateSoundTimer = 0
+
+  self.worldBottomDeathLevel = 5
 
   local ouchNoise = status.statusProperty("ouchNoise")
   if ouchNoise then
     animator.setSoundPool("ouch", {ouchNoise})
   end
-	
-	local bounds = mcontroller.boundBox() --Mcontroller for apex movement
+
+  self.inflictedDamage = damageListener("inflictedDamage", inflictedDamageCallback)
+
+  message.setHandler("applyStatusEffect", function(_, _, effectConfig, duration, sourceEntityId)
+      status.addEphemeralEffect(effectConfig, duration, sourceEntityId)
+    end)
+end
+
+function inflictedDamageCallback(notifications)
+  for _,notification in ipairs(notifications) do
+    if notification.killed then
+      if world.entityExists(notification.targetEntityId) then
+        local entityType = world.entityType(notification.targetEntityId)
+        local eventFields = entityEventFields(notification.targetEntityId)
+        util.mergeTable(eventFields, worldEventFields())
+        eventFields.damageSourceKind = notification.damageSourceKind
+
+        if entityType == "object" then
+          recordEvent(entity.id(), "killObject", eventFields)
+
+        elseif entityType == "npc" or entityType == "monster" or entityType == "player" then
+          recordEvent(entity.id(), "kill", eventFields)
+        end
+      else
+        -- TODO: better method for getting data on killed entities
+        sb.logInfo("Skipped event recording for nonexistent entity %s", notification.targetEntityId)
+      end
+    end
+  end
 end
 
 function applyDamageRequest(damageRequest)
@@ -30,7 +61,7 @@ function applyDamageRequest(damageRequest)
   elseif damageRequest.damageType == "IgnoresDef" then
     damage = damage + damageRequest.damage
   end
-  
+
   if status.resourcePositive("damageAbsorption") then
     local damageAbsorb = math.min(damage, status.resource("damageAbsorption"))
     status.modifyResource("damageAbsorption", -damageAbsorb)
@@ -47,20 +78,20 @@ function applyDamageRequest(damageRequest)
       end
     end
 
-		status.setResourcePercentage("shieldStaminaRegenBlock", 1.0)
-		damage = 0
-		damageRequest.statusEffects = {}
-		damageRequest.damageSourceKind = "shield"
-	end
-		
-  local damageHealthPercentage = damage / status.resourceMax("health")
+    status.setResourcePercentage("shieldStaminaRegenBlock", 1.0)
+    damage = 0
+    damageRequest.statusEffects = {}
+    damageRequest.damageSourceKind = "shield"
+  end
 
-  if damage > 0 and damageRequest.damageType ~= "Knockback" then
-    status.modifyResource("health", -damage)
-    self.damageFlashTime = 0.07
+  local healthLost = math.min(damage, status.resource("health"))
+  if healthLost > 0 and damageRequest.damageType ~= "Knockback" then
+    status.modifyResource("health", -healthLost)
     animator.playSound("ouch")
+
+    local damageHealthPercentage = damage / status.resourceMax("health")
     if damageHealthPercentage > status.statusProperty("hitInvulnerabilityThreshold") then
-      self.hitInvulnerabilityTime = status.statusProperty("hitInvulnerabilityTime") * math.min(damageHealthPercentage, 1.0)
+      self.hitInvulnerabilityTime = status.statusProperty("hitInvulnerabilityTime")
     end
   end
 
@@ -70,26 +101,18 @@ function applyDamageRequest(damageRequest)
 
   local knockbackMomentum = vec2.mul(damageRequest.knockbackMomentum, knockbackFactor)
   local knockback = vec2.mag(knockbackMomentum)
-  if knockback > 0 then
+  if knockback > status.stat("knockbackThreshold") then
     mcontroller.setVelocity({0,0})
-    if mcontroller.baseParameters().gravityEnabled then
-      local dir = knockbackMomentum[1] > 0 and 1 or -1
-      mcontroller.addMomentum({dir * knockback / 1.41, knockback / 1.41})
-    else
-      mcontroller.addMomentum(knockbackMomentum)
-    end
+    local dir = knockbackMomentum[1] > 0 and 1 or -1
+    mcontroller.addMomentum({dir * knockback / 1.41, knockback / 1.41})
   end
 
-	--Explode on death
-	if not status.resourcePositive("health") and status.statPositive("explosionDeath") then
-		world.spawnProjectile("zbomb", mcontroller.position(), 0, {0, 0}, false, { timeToLive = 0 })
-	end
-	
   return {{
     sourceEntityId = damageRequest.sourceEntityId,
     targetEntityId = entity.id(),
     position = mcontroller.position(),
-    damage = damage,
+    damageDealt = damage,
+    healthLost = healthLost,
     hitType = damageRequest.hitType,
     damageSourceKind = damageRequest.damageSourceKind,
     targetMaterialKind = status.statusProperty("targetMaterialKind"),
@@ -110,25 +133,10 @@ function update(dt)
   local baseGravity = 80
   local gravityDiffFactor = 1 / 30.0
 
-	-- Adjusts avian fall damage
---	if world.entitySpecies(entity.id()) == "avian" then
---		local minimumFallDistance = 21
---		local minimumFallVel = 60
---	end
-	
-	-- Adjusts apex fall damage
---	if world.entitySpecies(entity.id()) == "apex" then
---		local minimumFallDistance = 17.5
---		local minimumFallVel = 50
---	end
-	
   local curYPosition = mcontroller.yPosition()
   local yPosChange = curYPosition - (self.lastYPosition or curYPosition)
 
-  local curYVelocity = yPosChange / dt
-  local yVelChange = curYVelocity - self.lastYVelocity
-
-  if self.fallDistance > minimumFallDistance and yVelChange > minimumFallVel  and mcontroller.onGround() then
+  if self.fallDistance > minimumFallDistance and -self.lastYVelocity > minimumFallVel and mcontroller.onGround() then
     local damage = (self.fallDistance - minimumFallDistance) * fallDistanceDamageFactor
     damage = damage * (1.0 + (world.gravity(mcontroller.position()) - baseGravity) * gravityDiffFactor)
     damage = damage * status.stat("fallDamageMultiplier")
@@ -140,14 +148,14 @@ function update(dt)
       })
   end
 
-  if curYVelocity < -minimumFallVel then
+  if mcontroller.yVelocity() < -minimumFallVel and not mcontroller.onGround() then
     self.fallDistance = self.fallDistance + -yPosChange
   else
     self.fallDistance = 0
   end
 
   self.lastYPosition = curYPosition
-  self.lastYVelocity = curYVelocity
+  self.lastYVelocity = mcontroller.yVelocity()
 
 	--Human
 	if world.entitySpecies(entity.id()) == "human" then
@@ -190,12 +198,17 @@ function update(dt)
 	--Novakid
 	if world.entitySpecies(entity.id()) == "novakid" then
 		status.addEphemeralEffect("racenovakid",math.huge)
-		status.addEphemeralEffect("xenonglow",math.huge)
+		--status.addEphemeralEffect("xenonglow",math.huge)
+	end
+	
+	--Novakid death explosion
+	if not status.resourcePositive("health") and world.entitySpecies(entity.id()) == "novakid" then
+		world.spawnProjectile("zbomb", mcontroller.position(), 0, {0, 0}, false, { timeToLive = 0 })
 	end
 	
   local mouthPosition = vec2.add(mcontroller.position(), status.statusProperty("mouthPosition"))
   if status.statPositive("breathProtection") or world.breathable(mouthPosition) 
-	or status.statPositive("waterbreathProtection") and world.liquidAt(mouthPosition) 
+		or status.statPositive("waterbreathProtection") and world.liquidAt(mouthPosition) 
 	then
     status.modifyResource("breath", status.stat("breathRegenerationRate") * dt)
   else
@@ -212,15 +225,16 @@ function update(dt)
   else
     self.suffocateSoundTimer = 0
   end
-	
+
   self.hitInvulnerabilityTime = math.max(self.hitInvulnerabilityTime - dt, 0)
   local flashTime = status.statusProperty("hitInvulnerabilityFlash")
-  self.damageFlashTime = math.max(0, self.damageFlashTime - dt)
 
-  if self.hitInvulnerabilityTime > 0 and math.fmod(self.hitInvulnerabilityTime, flashTime) > flashTime / 2 then
-    status.setPrimaryDirectives("multiply=ffffff00")
-  elseif self.damageFlashTime > 0 then
-    status.setPrimaryDirectives("fade=ff0000=0.85")
+  if self.hitInvulnerabilityTime > 0 then
+    if math.fmod(self.hitInvulnerabilityTime, flashTime) > flashTime / 2 then
+      status.setPrimaryDirectives(status.statusProperty("damageFlashOffDirectives"))
+    else
+      status.setPrimaryDirectives(status.statusProperty("damageFlashOnDirectives"))
+    end
   else
     status.setPrimaryDirectives()
   end
@@ -240,8 +254,8 @@ function update(dt)
     status.setResourceLocked("energy", false)
   end
 
-  if not status.resourcePositive("energyRegenBlock") and status.resourcePositive("energylightRegen") then --Light-based energy regen
-		if world.lightLevel(mouthPosition) > 0.7 then
+  if not status.resourcePositive("energyRegenBlock") then
+			if world.lightLevel(mouthPosition) > 0.7 then
 			status.modifyResourcePercentage("energy", 0.019)
 		elseif world.lightLevel(mouthPosition) > 0.65 then
 			status.modifyResourcePercentage("energy", 0.017)
@@ -272,43 +286,28 @@ function update(dt)
     status.modifyResourcePercentage("energy", status.stat("energyRegenPercentageRate") * dt)
   end
 
-	self.shieldHitInvulnerabilityTime = math.max(self.shieldHitInvulnerabilityTime - dt, 0)
+  self.shieldHitInvulnerabilityTime = math.max(self.shieldHitInvulnerabilityTime - dt, 0)
   if not status.resourcePositive("shieldStaminaRegenBlock") then
     status.modifyResourcePercentage("shieldStamina", status.stat("shieldStaminaRegen") * dt)
     status.modifyResourcePercentage("perfectBlockLimit", status.stat("perfectBlockLimitRegen") * dt)
   end
-	
-	--Light-based health regen
-	if status.resourcePositive("healthlightRegen") then
-		if world.lightLevel(mouthPosition) > 0.6 then
-			status.modifyResourcePercentage("health", 0.0008)
-		elseif world.lightLevel(mouthPosition) > 0.55 then
-			status.modifyResourcePercentage("health", 0.0007)
-		elseif world.lightLevel(mouthPosition) > 0.5 then
-			status.modifyResourcePercentage("health", 0.0006)
-		elseif world.lightLevel(mouthPosition) > 0.45 then
-			status.modifyResourcePercentage("health", 0.0005)
-		elseif world.lightLevel(mouthPosition) > 0.4 then
-			status.modifyResourcePercentage("health", 0.0004)
-		elseif world.lightLevel(mouthPosition) > 0.35 then
-				status.modifyResourcePercentage("health", 0.0003)
-		elseif world.lightLevel(mouthPosition) > 0.3 then
-			status.modifyResourcePercentage("health", 0.0002)
-		elseif world.lightLevel(mouthPosition) > 0.25 then
-			status.modifyResourcePercentage("health", 0.0001)
-		elseif world.lightLevel(mouthPosition) > 0.2 then
-			status.modifyResourcePercentage("health", 0.00005)
-		end
-	end
+
+  self.inflictedDamage:update()
+
+  if mcontroller.position()[2] < self.worldBottomDeathLevel then
+    status.setResourcePercentage("health", 0)
+  end
 end
 
 function overheadBars()
+  local bars = {}
+
   if status.statPositive("shieldHealth") then
-    return {{
+    table.insert(bars, {
       percentage = status.resource("shieldStamina"),
       color = status.resourcePositive("perfectBlock") and {255, 255, 200, 255} or {200, 200, 0, 255}
-    }}
+    })
   end
 
-  return {}
+  return bars
 end
